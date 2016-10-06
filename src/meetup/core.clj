91395a103bcb4295ce628a86074bf1bf1fcfcaa5
Jochen Rau <jrau@infinitecloud.com>
2016-10-06 15:03:16 -0400

    Remove server

--- src/meetup/core.clj
+++ src/meetup/core.clj
@@ -1,16 +1,7 @@
-(ns meetup.core
-  (:require
-    [aleph.http :refer [start-server]]))
+(ns meetup.core)
 
 (def web-app
   (fn [request]
-    (clojure.pprint/pprint request)
     {:status  200
      :headers {"Content-Type" "text/html"}
      :body    "<h1>Hello World</h1>"}))
-
-(defn start []
-  (start-server #'web-app {:port 8080}))
-
-(defn stop [server]
-  (.close server))
