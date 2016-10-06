39036b4c4b61da4a8b4a13c753829db0afb739e0
Jochen Rau <jrau@infinitecloud.com>
2016-10-06 14:54:18 -0400

    Remove routing

--- src/meetup/core.clj
+++ src/meetup/core.clj
@@ -2,26 +2,12 @@
   (:require
     [aleph.http :refer [start-server]]))
 
-(def routes
-  {"/"         (fn [request]
-                 {:status  200
-                  :headers {"Content-Type" "text/html"}
-                  :body    "<h1>Hello World</h1><a href=\"/products\">Products</a>"})
-   "/products" (fn [request]
-                 {:status  200
-                  :headers {"Content-Type" "text/html"}
-                  :body    "<h1>Products Page</h1>"})})
-
 (def web-app
   (fn [request]
     (clojure.pprint/pprint request)
-    (let [uri (:uri request)
-          handler (or (get routes uri)
-                      (fn [request]
-                        {:status  404
-                         :headers {"Content-Type" "text/html"}
-                         :body    "Nothing here. That's sad."}))]
-      (handler request))))
+    {:status  200
+     :headers {"Content-Type" "text/html"}
+     :body    "<h1>Hello World</h1>"}))
 
 (defn start []
   (start-server #'web-app {:port 8080}))
