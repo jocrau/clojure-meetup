e01c29f09cd8f39f002e4fb1ed6f8eeb63e31075
Jochen Rau <jrau@infinitecloud.com>
2016-10-06 14:51:56 -0400

    Inline responses

--- src/meetup/core.clj
+++ src/meetup/core.clj
@@ -2,31 +2,25 @@
   (:require
     [aleph.http :refer [start-server]]))
 
-(def not-found-response
-  {:status 404
-   :headers {"Content-Type" "text/html"}
-   :body   "Nothing here. That's sad."})
-
-(def home-response
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body   "<h1>Hello World</h1><a href=\"/products\">Products</a>"})
-
-(def products-response
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body   "<h1>Products Page</h1>"})
-
 (def routes
-  {"/"         (fn [request] home-response)
-   "/products" (fn [request] products-response)})
+  {"/"         (fn [request]
+                 {:status  200
+                  :headers {"Content-Type" "text/html"}
+                  :body    "<h1>Hello World</h1><a href=\"/products\">Products</a>"})
+   "/products" (fn [request]
+                 {:status  200
+                  :headers {"Content-Type" "text/html"}
+                  :body    "<h1>Products Page</h1>"})})
 
 (def web-app
   (fn [request]
     (clojure.pprint/pprint request)
     (let [uri (:uri request)
           handler (or (get routes uri)
-                      (fn [request] not-found-response))]
+                      (fn [request]
+                        {:status  404
+                         :headers {"Content-Type" "text/html"}
+                         :body    "Nothing here. That's sad."}))]
       (handler request))))
 
 (defn start []
