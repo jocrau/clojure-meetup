2a0ea170e668afede4aec03bf5517e952ceecd46
Jochen Rau <jrau@infinitecloud.com>
2016-10-06 15:09:59 -0400

    Implement unit test

---
+++ test/meetup/core_test.clj
@@ -0,0 +1,19 @@
+(ns meetup.core-test
+  (:require [clojure.test :refer :all]
+            [meetup.core :refer :all]))
+
+(def test-request
+  {:scheme         :http
+   :request-method :get
+   :remote-addr    "127.0.0.1"
+   :server-port    8080
+   :uri            "/products"
+   :query-string   nil
+   :headers        {}
+   :body           nil})
+
+(deftest web-app-test
+  (testing "simple request-response cycle"
+    (let [response (web-app test-request)]
+      (is (= (get response :status) 200))
+      (is (string? (get response :body))))))
