873972dd7594ac4785b6bc72dd57a22b33869c4b
Jochen Rau <jrau@infinitecloud.com>
2016-10-06 23:03:12 -0400

    Refactoring: abstract how we build a handler

--- src/meetup/core.clj
+++ src/meetup/core.clj
@@ -1,14 +1,20 @@
 (ns meetup.core)
 
+(defn handler [resource]
+  (fn [request]
+    {:status  200
+     :headers {"Content-Type" "text/html"}
+     :body    (get resource :html)}))
+
+(def home-resource
+  {:html "<h1>Hello World</h1><a href=\"/products\">Products</a>"})
+
+(def product-resource
+  {:html "<h1>Products Page</h1>"})
+
 (def routes
-  {"/"         (fn [request]
-                 {:status  200
-                  :headers {"Content-Type" "text/html"}
-                  :body    "<h1>Hello World</h1><a href=\"/products\">Products</a>"})
-   "/products" (fn [request]
-                 {:status  200
-                  :headers {"Content-Type" "text/html"}
-                  :body    "<h1>Products Page</h1>"})})
+  {"/"         (handler home-resource)
+   "/products" (handler product-resource)})
 
 (def not-found-response
   {:status 404})
