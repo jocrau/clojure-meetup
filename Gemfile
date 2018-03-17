d7851d7d78e4c760a5dc347a51ef59fd5450537e
Jochen Rau <jrau@infinitecloud.com>
2018-03-17 16:03:59 -0400

    Update dependencies

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'http://rubygems.org'
 
 gem 'asciidoctor-revealjs' # latest released version
 #gem 'asciidoctor-revealjs', github: 'asciidoctor/asciidoctor-reveal.js' # github master branch
+gem 'asciidoctor-diagram', '~> 1.4.0'
--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :source-paths ["src/clj" "src/cljs" "src/cljc"]
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.9.946"]
+                 [org.clojure/clojurescript "1.10.145"]
 
                  ; web server
                  [http-kit "2.2.0"]
@@ -13,11 +13,12 @@
 
                  ; request handling
                  [yada "1.2.11"]
-                 [liberator "0.14.1"]
+                 [liberator "0.15.1"]
+                 [cljs-http "0.1.44"]
 
                  ; routing
                  [bidi "2.1.3"]
-                 [compojure "1.5.1"]
+                 [compojure "1.6.0"]
 
                  ; security
                  [buddy/buddy "2.0.0"]
@@ -28,27 +29,44 @@
                  [enlive "1.1.6"]
                  [de.ubercode.clostache/clostache "1.4.0"]
 
-                 [reagent "0.8.0-alpha1"]
+                 [reagent "0.8.0-alpha2"]
 
                  [org.clojure/tools.namespace "0.2.11"]]
 
-  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.14"]
-                                  [binaryage/devtools "0.9.9"]]
+  :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.15"]
+                                  [binaryage/devtools "0.9.9"]
+                                  [viebel/gadjett "0.5.2"]
+                                  [klipse "7.3.2" :exclusions [viebel/gadjett]]]
                    :plugins      [[lein-cljsbuild "1.1.7"]
-                                  [lein-figwheel "0.5.14" :exclusions [org.clojure/clojure org.clojure/clojurescript]]]}}
+                                  [lein-figwheel "0.5.15" :exclusions [org.clojure/clojure org.clojure/clojurescript]]]}}
 
-  :cljsbuild {:builds {:dev {:figwheel     {:on-jsload "tutorials.react_frontend.app/init"}
-                             :source-paths ["src/cljs" "src/cljc"]
-                             :compiler     {:main                 tutorials.react_frontend.app
-                                            :asset-path           "js/compiled"
-                                            :output-to            "resources/js/reagent_frontend.js"
-                                            :output-dir           "resources/js/compiled"
-                                            :optimizations        :none
-                                            :preloads             [devtools.preload]
-                                            :source-map-timestamp true
-                                            :pretty-print         true}}}}
+  :cljsbuild {:builds {:dev    {:figwheel     {:on-jsload "tutorials.react_frontend.app/init"}
+                                :source-paths ["src/cljs" "src/cljc"]
+                                :compiler     {:main                 tutorials.react_frontend.app
+                                               :asset-path           "js/tutorials/compiled"
+                                               :output-to            "resources/js/tutorials/react_frontend.js"
+                                               :output-dir           "resources/js/tutorials/compiled"
+                                               :optimizations        :none
+                                               :npm-deps             {:react     "16.1.0"
+                                                                      :react-dom "16.1.0"}
+                                               :install-deps         true
+                                               :preloads             [devtools.preload]
+                                               :source-map-timestamp true
+                                               :pretty-print         true}}
+                       :klipse {:figwheel     true
+                                :source-paths ["dev-resources/cljs"]
+                                :compiler     {:main               klipse-meetup.plugin
+                                               :asset-path         "../../resources/js/klipse/compiled"
+                                               :output-to          "resources/js/klipse/klipse_plugin.js"
+                                               :output-dir         "resources/js/klipse/compiled"
+                                               :optimizations      :none
+                                               :pretty-print       true
+                                               :verbose            true}}}}
 
   :figwheel {:css-dirs ["resources/css"]}
+
+  :clean-targets ^{:protect false} ["resources/js/tutorials"
+                                    "resources/js/klipse"]
 
   :main ^:skip-aot meetup.intro
   :target-path "target/%s")
