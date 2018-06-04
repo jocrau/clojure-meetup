4e8ef2ba488f3c131910a6b7fc5316e00f971c54
Jochen Rau <jrau@infinitecloud.com>
2018-06-04 13:53:47 -0400

    Remove compiled Klipse

--- resources/js/klipse/compiled/klipse/tools.js
+++
@@ -1,9 +0,0 @@
-// Compiled by ClojureScript 1.9.946 {}
-goog.provide('klipse.tools');
-goog.require('cljs.core');
-goog.require('cljs.js');
-klipse.tools.eval = (function klipse$tools$eval(form){
-return new cljs.core.Keyword(null,"value","value",305978217).cljs$core$IFn$_invoke$arity$1(cljs.js.eval.call(null,cljs.js.empty_state.call(null),form,cljs.core.identity));
-});
-
-//# sourceMappingURL=tools.js.map
