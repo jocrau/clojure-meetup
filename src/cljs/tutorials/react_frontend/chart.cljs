a52d17ba38936e28d538e90f40187faded907c15
Jochen Rau <jrau@infinitecloud.com>
2018-06-19 16:44:31 -0400

    Pass a function as props

--- src/cljs/tutorials/react_frontend/chart.cljs
+++ src/cljs/tutorials/react_frontend/chart.cljs
@@ -6,16 +6,16 @@
 (defn props->option [{:keys [data interval] :as props}]
   (clj->js {:xAxis     {:show true :type "time" :min (:start interval) :max (:end interval) :splitLine {:show false}}
             :yAxis     {:show true :min 0 :max 20}
-            :animation true
+            :animation false
             :series    [{:type "line" :data data}]}))
 
-(defn timeline [{:keys [size] :or {size "300px"} :as props}]
+(defn timeline [{:keys [size interval] :or {size "300px"} :as props}]
   (r/create-class
-    {:reagent-render       (fn [this]
-                             [:span {:style {:width size :height "200px"}}])
+    {:reagent-render       (fn [props]
+                             [:span {:style {:width size :height "180px"}}])
      :component-did-mount  (fn [this]
                              (doto (js/echarts.init (r/dom-node this))
                                (.setOption (props->option props))))
-     :component-will-update (fn [this [_ props]]
+     :component-did-update (fn [this [_ props]]
                              (doto (js/echarts.getInstanceByDom (r/dom-node this))
                                (.setOption (props->option props) true)))}))
